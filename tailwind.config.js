dc7d1e7632538531a8c9edd8eadfb87ccc9e2901
Dan Mullany <scillfury@gmail.com>
2023-03-22 11:54:44 +0000

    add abis json files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
-  theme: {
-    extend: {
-      colors: {
-        "brand-green": "#10DCC1",
-        "brand-black": {
-          light: "#23262B",
-          DEFAULT: "#080808",
-        },
-        "brand-blue": "#29E5FF",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./app/**/*.{ts,tsx,jsx,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"brand-green": "#10DCC1",
+				"brand-black": {
+					light: "#23262B",
+					DEFAULT: "#080808",
+				},
+				"brand-blue": "#29E5FF",
+			},
+		},
+	},
+	plugins: [],
 };
