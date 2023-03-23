5c54e29dd2584d45a3d524f5abc99f51c7f43e9d
mmarchukomi <128700477+mmarchukomi@users.noreply.github.com>
2023-03-23 18:20:29 +0200

    Update WalletConnectArea.tsx

--- app/components/header-components/connect-wallet/WalletConnectArea.tsx
+++ app/components/header-components/connect-wallet/WalletConnectArea.tsx
@@ -1,10 +1,8 @@
 import ConnectWallet from "./connect-wallet";
-import NetworksDropdown from "./networksDropdown";
 
 export const WalletConnectArea = () => {
   return (
     <>
-      <NetworksDropdown />
       <ConnectWallet />
     </>
   );
